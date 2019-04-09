f33c1ec1605d90806bf2534bb5148b9ac0cf126d
riccardooliva <riccardo.oliva@drivek.com>
2019-04-09 15:32:27 +0200

    Added set and get

--- src/Media/MediaTag.php
+++ src/Media/MediaTag.php
@@ -19,11 +19,6 @@ class MediaTag {
 	protected $tag = '';
 
 	/**
-	 * @var string
-	 */
-	protected $src = '';
-
-	/**
 	 * @var array
 	 */
 	protected $meta = [];
@@ -34,7 +29,7 @@ class MediaTag {
 	 * @param array $media_options = []
 	 */
 	public function __construct( array $media_options = [] ) {
-		$this->media_options  = $media_options;
+		$this->media_options = $media_options;
 	}
 
 	/**
@@ -46,15 +41,24 @@ class MediaTag {
 	 * @return MediaTag
 	 */
 	public function __set( string $name, $value ): MediaTag {
-		if('tag' == $name && is_string($value)) {
+		if ( 'tag' == $name && is_string( $value ) ) {
 			$this->tag = strtolower( $value );
-		} elseif ('src' == $name && is_string($value)) {
-			$this->src = $value;
 		} else {
-			//
+			$this->meta[ $this->handle_camel_case( $name ) ] = $value;
 		}
 
 		return $this;
+	}
+
+	/**
+	 * Dynamic property get
+	 *
+	 * @param string $name
+	 *
+	 * @return mixed
+	 */
+	public function __get( string $name ) {
+		return $this->meta[ $name ] ?? null;
 	}
 
 	/**
@@ -63,6 +67,23 @@ class MediaTag {
 	 * @return string
 	 */
 	public function __toString(): string {
-		// TODO: Implement __toString() method.
+		$data = [];
+		foreach ( $this->meta as $key => $value ) {
+			$data[] = sprintf( '%s="%s"' );
+		}
+		$data[] = '/';
+
+		return sprintf( '<%s%s/>', $this->tag, empty( $data ) ? ' ' : implode( ' ', $data ) );
+	}
+
+	/**
+	 * Converts a camel case string to multiple words separated by '-'
+	 *
+	 * @param string $name
+	 *
+	 * @return string
+	 */
+	protected function handle_camel_case( string $name ): string {
+		return strtolower( implode( '-', preg_split( '/(?=[A-Z])/', $name ) ) );
 	}
 }
